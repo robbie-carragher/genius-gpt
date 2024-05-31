46c02b2480725f7a3f595238f33c36654e123f47
robbie-carragher <orcafinweb@gmail.com>
2024-05-31 16:28:32 -0700

    63. Sidebar

--- genius-gpt/components/SideBar.jsx
+++ genius-gpt/components/SideBar.jsx
@@ -1,9 +1,17 @@
-import React from 'react'
+import SidebarHeader from './SidebarHeader';
+import NavLinks from './NavLinks';
+import MemberProfile from './MemberProfile';
 
-const SideBar = () => {
+const Sidebar = () => {
   return (
-    <div className='text-6xl'>SideBar</div>
-  )
-}
-
-export default SideBar
+    <div className='px-4 w-80 min-h-full bg-base-300 py-12 grid grid-rows-[auto,1fr,auto] '>
+      {/* first row */}
+      <SidebarHeader />
+      {/* second row */}
+      <NavLinks />
+      {/* third row */}
+      <MemberProfile />
+    </div>
+  );
+};
+export default Sidebar;

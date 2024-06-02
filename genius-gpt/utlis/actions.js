94a5aeff219007a8dafe1c3d5676e680b6d45dc3
robbie-carragher <orcafinweb@gmail.com>
2024-06-02 01:11:13 -0700

    79.Completed

--- genius-gpt/utlis/actions.js
+++ genius-gpt/utlis/actions.js
@@ -1,4 +1,35 @@
 
+// 'use server';
+// import OpenAI from 'openai';
+
+// if (!process.env.OPENAI_API_KEY) {
+//   console.error("OPENAI_API_KEY is not set");
+// } else {
+//   console.log("OPENAI_API_KEY:", process.env.OPENAI_API_KEY);
+// }
+
+// const openai = new OpenAI({
+//   apiKey: process.env.OPENAI_API_KEY,
+// });
+
+// export const generateChatResponse = async (chatMessage) => {
+//   try {
+//     const response = await openai.chat.completions.create({
+//       messages: [
+//         { role: 'system', content: 'You are a helpful assistant' },
+//         { role: 'user', content: chatMessage },
+//       ],
+//       model: 'gpt-3.5-turbo',
+//       temperature: 0,
+//     });
+//     console.log(response);
+//     console.log(response.choices[0].message);
+//     return response.choices[0].message;
+//   } catch (error) {
+//     console.error('Error generating chat response:', error);
+//     return null;
+//   }
+// };
 'use server';
 import OpenAI from 'openai';
 
@@ -12,18 +43,16 @@ const openai = new OpenAI({
   apiKey: process.env.OPENAI_API_KEY,
 });
 
-export const generateChatResponse = async (chatMessage) => {
+export const generateChatResponse = async (chatMessages) => {
   try {
     const response = await openai.chat.completions.create({
       messages: [
         { role: 'system', content: 'You are a helpful assistant' },
-        { role: 'user', content: chatMessage },
+        ...chatMessages,
       ],
       model: 'gpt-3.5-turbo',
       temperature: 0,
     });
-    console.log(response);
-    console.log(response.choices[0].message);
     return response.choices[0].message;
   } catch (error) {
     console.error('Error generating chat response:', error);

150afb5f8e974c6315ddd74aae92eceac0027c62
robbie-carragher <orcafinweb@gmail.com>
2024-06-02 16:18:37 -0700

    82.complete

--- genius-gpt/components/NewTour.jsx
+++ genius-gpt/components/NewTour.jsx
@@ -1,9 +1,45 @@
-import React from 'react'
+'use client';
+
+import TourInfo from "./TourInfo";
 
 const NewTour = () => {
-  return (
-    <div>NewTour</div>
-  )
-}
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const formDat = new FormData(e.currentTarget);
+    const destination = Object.fromEntries(formDat.entries())
+    console.log(destination);
+    // Add your submission logic here
+  };
 
-export default NewTour
+  return (
+    <>
+      <form onSubmit={handleSubmit} action="" className="max-w-2xl">
+        <h2 className="mb-4">Select your Dream Destination</h2>
+        <div className="join w-full">
+          <input
+            type="text"
+            className="input input-bordered join-item w-full"
+            placeholder="city"
+            name="city"
+            required
+          />
+          <input
+            type="text"
+            className="input input-bordered join-item w-full"
+            placeholder="country"
+            name="country"
+            required
+          />
+          <button className="btn btn-primary join-item" type="submit">
+            generate tour
+          </button>
+        </div>
+      </form>
+      <div className="mt-16">
+        <TourInfo />
+      </div>
+    </>
+  );
+};
+
+export default NewTour;

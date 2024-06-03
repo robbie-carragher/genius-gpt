de2c6ec35404c743260cc0bac7b177f683db3b13
robbie-carragher <orcafinweb@gmail.com>
2024-06-02 20:00:58 -0700

    83.84.85.Completed

--- genius-gpt/components/TourInfo.jsx
+++ genius-gpt/components/TourInfo.jsx
@@ -1,9 +1,33 @@
-import React from 'react'
+// import React from 'react'
 
-const TourInfo = () => {
+// const TourInfo = ({tour}) => {
+//     console.log(tour)
+//   return (
+//     <div>{tour}</div>
+//   )
+// }
+
+// export default TourInfo
+
+import React from 'react';
+
+const TourInfo = ({ tour }) => {
+  if (!tour) {
+    return null;
+  }
+
   return (
-    <div>TourInfo</div>
-  )
-}
+    <div className="tour-info">
+      <h2 className="text-2xl font-bold">{tour.title}</h2>
+      <p>{tour.description}</p>
+      <h3 className="text-xl font-semibold mt-4">Stops:</h3>
+      <ul className="list-disc ml-5">
+        {tour.stops.map((stop, index) => (
+          <li key={index}>{stop}</li>
+        ))}
+      </ul>
+    </div>
+  );
+};
 
-export default TourInfo
+export default TourInfo;

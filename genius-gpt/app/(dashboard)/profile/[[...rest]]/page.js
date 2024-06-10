bcb81c7d9006a696732fc5c93495bb140bc5b894
robbie-carragher <orcafinweb@gmail.com>
2024-06-10 00:02:24 -0700

    profile bug fixed

--- genius-gpt/app/(dashboard)/profile/[[...rest]]/page.js
+++ genius-gpt/app/(dashboard)/profile/[[...rest]]/page.js
@@ -1,27 +1,11 @@
 
-
 import { UserProfile } from '@clerk/nextjs';
-const UserProfilePage = () => {
-  return <UserProfile />;
-};
+
+const UserProfilePage = () => (
+  <div>
+    <UserProfile routing="path" path="/profile" />
+  </div>
+);
+
 export default UserProfilePage;
 
-// import { UserProfile } from '@clerk/nextjs';
-
-// const UserProfilePage = () => (
-//   <div>
-//     <UserProfile routing="path" path="/profile" />
-//   </div>
-// );
-
-// export default UserProfilePage;
-
-// import { UserProfile } from '@clerk/nextjs';
-
-// const UserProfilePage = () => (
-//   <div>
-//     <UserProfile routing="path" path="/profile" />
-//   </div>
-// );
-
-// export default UserProfilePage;
--- genius-gpt/middleware.ts
+++ genius-gpt/middleware.ts
@@ -1,35 +1,3 @@
-// import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server';
- 
-// const isProtectedRoute = createRouteMatcher([
-//   '/chat(.*)',
-//   '/profile(.*)',
-//   '/chat(.*)',
-//   '/tours(.*)',
-// ]);
- 
-// export default clerkMiddleware((auth, req) => {
-//   if (isProtectedRoute(req)) auth().protect();
-// });
- 
-// export const config = {
-//   matcher: ['/((?!.*\\..*|_next).*)', '/', '/(api|trpc)(.*)'],
-// };
-
-// import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server';
-
-// const isProtectedRoute = createRouteMatcher([
-//   '/chat(.*)',
-//   '/profile(.*)',
-//   '/tours(.*)',
-// ]);
-
-// export default clerkMiddleware((auth, req) => {
-//   if (isProtectedRoute(req)) auth().protect();
-// });
-
-// export const config = {
-//   matcher: ['/((?!.*\\..*|_next).*)', '/', '/(api|trpc)(.*)'],
-// };
 
 import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server';
 
@@ -46,3 +14,5 @@ export default clerkMiddleware((auth, req) => {
 export const config = {
   matcher: ['/((?!.*\\..*|_next).*)', '/', '/(api|trpc)(.*)'],
 };
+
+

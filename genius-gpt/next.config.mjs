174d395e4ae9fd848dd313231c3c17b6f0ce3dd9
robbie-carragher <orcafinweb@gmail.com>
2024-06-15 18:14:04 -0700

    testing

--- genius-gpt/next.config.mjs
+++ genius-gpt/next.config.mjs
@@ -17,7 +17,9 @@ const nextConfig = {
     ],
   },
     env: {
-        OPENAI_API_KEY: process.env.OPENAI_API_KEY,
+      NEXT_PUBLIC_SUPABASE_URL: process.env.NEXT_PUBLIC_SUPABASE_URL,
+      NEXT_PUBLIC_SUPABASE_ANON_KEY: process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY,
+      OPENAI_API_KEY: process.env.OPENAI_API_KEY,
       },
 };
 
